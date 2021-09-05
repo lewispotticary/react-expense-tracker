8afc2c115f17121424a5857f3b2a217b3e254fd1
Lewis Potticary <lewispotticary@gmail.com>
2021-09-05 21:03:28 +0100

    Added remove expense from list function

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-function Table ({expenseName, amountValue, categoryValue, dateValue}){
+function Table ({expenseName, amountValue, categoryValue, dateValue, setExpenseList, expenseList, id, expenseObject}){
+
+    const deleteHandler = () => {
+        setExpenseList(expenseList.filter(el => el.id !== expenseObject.id))
+    };
+
     return(
             <tr>
                 <td>{expenseName}</td>
                 <td>{amountValue}</td>
                 <td>{categoryValue}</td>
                 <td>{dateValue}</td>
+                <td><button onClick={deleteHandler} className="trash-btn"><i className="fa fa-trash fa-lg"></i></button></td>
             </tr>
     )
 }
