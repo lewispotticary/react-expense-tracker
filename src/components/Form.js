afc51d7f336e27aa17f820dd7bcfbfdd195bf7b5
Lewis Potticary <lewispotticary@gmail.com>
2021-09-23 20:27:10 +0100

    Formatted Comments

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,87 +1,94 @@
 import React from 'react';
 
-function Form ({expenseName, setExpenseName, amount, setAmount, category, setCategory, date, setDate, expenseList, setExpenseList, filteredExpenses}){ //Form where user inputs expenses
+function Form ({expenseName, setExpenseName, amount, setAmount, category, setCategory, date, setDate, expenseList, setExpenseList}){ //Form where user inputs expenses
 
-    {/*Functions*/}
+    //Functions
 
-    {/*Expense Name Prop */}
-    const expenseNameHandler = (e) => {
-        setExpenseName(e.target.value)
-    }
+        //Expense Name Prop
 
-    {/*Amount Prop */}
+        const expenseNameHandler = (e) => {
+            setExpenseName(e.target.value)
+        }
 
-    const amountHandler = (e) => {
-        setAmount(e.target.value)
-    }
+        //Amount Prop
 
-    {/*Category Prop */}
+        const amountHandler = (e) => {
+            setAmount(e.target.value)
+        }
 
-    const categoryHandler = (e) => {
-        setCategory(e.target.value)
-    }
+        //Category Prop
 
-    {/*Date Prop */}
+        const categoryHandler = (e) => {
+            setCategory(e.target.value)
+        }
 
-    const dateHandler = (e) => {
-        setDate(e.target.value)
-    }
-    
-    {/*Add expense button function */}
+        //Date Prop
 
-    const submitExpenseHandler = (e) => {
-        e.preventDefault();
-        setExpenseList([
-            ...expenseList, {expenseName: expenseName, amountValue: amount, categoryValue: category, dateValue: date, id: Math.random() * 1000}
-        ])
-        setExpenseName("");
-        setAmount("");
-        setCategory("Bills");
-        setDate("");
-    }
+        const dateHandler = (e) => {
+            setDate(e.target.value)
+        }
+        
 
+        //Add expense button function. Create a class with form data and store into set expense list state. 
 
-    return(
-        <div className="formContainer">
+        const submitExpenseHandler = (e) => {
+            e.preventDefault();
+            setExpenseList([
+                ...expenseList, {expenseName: expenseName, amountValue: amount, categoryValue: category, dateValue: date, id: Math.random() * 1000}
+            ])
+            setExpenseName("");
+            setAmount("");
+            setCategory("Bills");
+            setDate("");
+        }
 
-            {/*Expense Name Container*/}
-            <div className="expenseName">
-                <h3>Expense Name</h3>
-                <input type="text" value={expenseName} onChange={expenseNameHandler}/>
+    //Main   
+
+        return(
+            <div className="formContainer">
+
+                {/*Expense Name Container*/}
+
+                <div className="expenseName">
+                    <h3>Expense Name</h3>
+                    <input type="text" value={expenseName} onChange={expenseNameHandler}/>
+                </div>
+
+                {/*Amount Container*/}
+
+                <div className="amount">
+                    <h3>Amount</h3>
+                    <input type="text" value={amount} onChange={amountHandler}/>
+                </div>
+
+                {/*Category Container*/}
+
+                <div className="category">
+                    <h3>Category</h3>
+                    <select id="cars" name="cars" value={category} onChange={categoryHandler}>
+                        <option value="Bills">Bills</option>
+                        <option value="Entertainment">Entertainment</option>
+                        <option value="Food">Food</option>
+                        <option value="Travel">Travel</option>
+                        <option value="Miscellaneous">Miscellaneous</option>
+                    </select>
+                </div>
+
+                {/*Date Container*/}
+                
+                <div className="date">
+                    <h3>Date</h3>
+                    <input type="date" value={date} onChange={dateHandler} />
+                </div>
+
+                <div className="lineBreak"></div>
+
+                <div className="expenseBtnCotainer">
+                    <button className="expenseBtn" onClick={submitExpenseHandler}>Add Expense</button>
+                </div>
+
             </div>
-
-            {/*Amount Container*/}
-            <div className="amount">
-                <h3>Amount</h3>
-                <input type="text" value={amount} onChange={amountHandler}/>
-            </div>
-
-            {/*Category Container*/}
-            <div className="category">
-                <h3>Category</h3>
-                <select id="cars" name="cars" value={category} onChange={categoryHandler}>
-                    <option value="Bills">Bills</option>
-                    <option value="Entertainment">Entertainment</option>
-                    <option value="Food">Food</option>
-                    <option value="Travel">Travel</option>
-                    <option value="Miscellaneous">Miscellaneous</option>
-                </select>
-            </div>
-
-            {/*Date Container*/}
-            <div className="date">
-                <h3>Date</h3>
-                <input type="date" value={date} onChange={dateHandler} />
-            </div>
-
-            <div className="lineBreak"></div>
-
-            <div className="expenseBtnCotainer">
-                <button className="expenseBtn" onClick={submitExpenseHandler}>Add Expense</button>
-            </div>
-
-        </div>
-    );
+        );
 }
 
 export default Form;
