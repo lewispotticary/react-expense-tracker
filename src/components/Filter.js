c2459e965f2847da28dc919ccf7d9401d26d05a6
Lewis Potticary <lewispotticary@gmail.com>
2021-09-23 20:11:56 +0100

    Added comments

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,45 +1,53 @@
 import React, {useEffect} from 'react';
 
-function Filter ({categoryFilter, setCategoryFilter, nameFilter, setNameFilter, setFilteredExpenses, expenseList}) {
+function Filter ({categoryFilter, setCategoryFilter, setNameFilter}) {
 
-    /*put use effect for name in app.js*/
+    //Functions
 
-    const categoryFilterHandler = (e) => {
-        setCategoryFilter(e.target.value)
-    }
+        //Set category filter state to event value
 
-    const nameFilterSet = (e) => {
-        setNameFilter(e.target.value);
-    }
+        const categoryFilterHandler = (e) => {
+            setCategoryFilter(e.target.value)
+        }
 
-    const resetHandler = () => {
-        setCategoryFilter("");
-        setNameFilter("");
-    }
+        //Set name filter state to event value
 
-    return(
-        <div className="filterContainer">
+        const nameFilterSet = (e) => {
+            setNameFilter(e.target.value);
+        }
 
-            <div className="nameFilter">
-                <h3>Name</h3>
-                <input type="text" onChange={nameFilterSet}/>
+        //Reset function that sets the state of category and name filter to empty
+
+        const resetHandler = () => {
+            setCategoryFilter("");
+            setNameFilter("");
+        }
+
+    //Main - Returns HTML
+
+        return(
+            <div className="filterContainer">
+
+                <div className="nameFilter">
+                    <h3>Name</h3>
+                    <input type="text" onChange={nameFilterSet}/>
+                </div>
+
+                <div className="categoryFilter">
+                    <h3>Category</h3>
+                    <select id="cars" name="cars" value={categoryFilter} onChange={categoryFilterHandler}>
+                        <option value="All">All</option>
+                        <option value="Bills">Bills</option>
+                        <option value="Entertainment">Entertainment</option>
+                        <option value="Food">Food</option>
+                        <option value="Travel">Travel</option>
+                        <option value="Miscellaneous">Miscellaneous</option>
+                    </select>
+                </div>
+
+                <button className="resetBtn" onClick={resetHandler}>Reset</button>
             </div>
-
-            <div className="categoryFilter">
-                <h3>Category</h3>
-                <select id="cars" name="cars" value={categoryFilter} onChange={categoryFilterHandler}>
-                    <option value="All">All</option>
-                    <option value="Bills">Bills</option>
-                    <option value="Entertainment">Entertainment</option>
-                    <option value="Food">Food</option>
-                    <option value="Travel">Travel</option>
-                    <option value="Miscellaneous">Miscellaneous</option>
-                </select>
-            </div>
-
-            <button className="resetBtn" onClick={resetHandler}>Reset</button>
-        </div>
-    )
+        )
 }
 
 export default Filter;
