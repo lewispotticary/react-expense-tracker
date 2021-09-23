7fc2846bc1a4f2898a8d321dee596fe541ca7d3e
Lewis Potticary <lewispotticary@gmail.com>
2021-09-23 20:19:07 +0100

    Added comments

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,20 +1,28 @@
 import React from 'react';
 
-function Table ({expenseName, amountValue, categoryValue, dateValue, setExpenseList, expenseList, id, expenseObject}){
+function Table ({expenseName, amountValue, categoryValue, dateValue, setExpenseList, expenseList, expenseObject}){
 
-    const deleteHandler = () => {
-        setExpenseList(expenseList.filter(el => el.id !== expenseObject.id))
-    };
+    //Functions
 
-    return(
-            <tr>
-                <td>{expenseName}</td>
-                <td>{amountValue}</td>
-                <td>{categoryValue}</td>
-                <td>{dateValue}</td>
-                <td><button onClick={deleteHandler} className="trash-btn"><i className="fa fa-trash fa-lg"></i></button></td>
-            </tr>
-    )
+        //Delete function. Filters expense list. If the clicked element id is not equal to object clicked then it is filtered out.
+
+        const deleteHandler = () => {
+            setExpenseList(expenseList.filter(el => el.id !== expenseObject.id))
+        };
+
+    //Main
+
+        //Mapped data from expense list is passed into new row and columns. 
+        
+        return(
+                <tr>
+                    <td>{expenseName}</td>
+                    <td>{amountValue}</td>
+                    <td>{categoryValue}</td>
+                    <td>{dateValue}</td>
+                    <td><button onClick={deleteHandler} className="trash-btn"><i className="fa fa-trash fa-lg"></i></button></td>
+                </tr>
+        )
 }
 
 export default Table;
